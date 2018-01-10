577b3c33eb6e77af8a784dac13248b891db3fa58
Artur Maslov <aartasas@gmail.com>
2018-01-10 16:15:24 +0200

    l7-ex4-add_set_preferences_resources

--- Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.03-Exercise-ReadingFromSharedPreferences/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -53,9 +53,9 @@ public class VisualizerActivity extends AppCompatActivity {
         // COMPLETE (2) Get a reference to the default shared preferences from the PreferenceManager class
         // COMPLETE (3) Get the value of the show_bass checkbox preference and use it to call setShowBass
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        Boolean showBassOrNo = sharedPreferences.getBoolean("show_bass", true);
+        Boolean bassPreference = sharedPreferences.getBoolean("show_bass", true);
 
-        mVisualizerView.setShowBass(showBassOrNo);
+        mVisualizerView.setShowBass(bassPreference);
         mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
