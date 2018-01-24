e2e3f3d0454cb64856123478e6ab83739fc1c768
Artur Maslov <aartasas@gmail.com>
2018-01-25 00:57:50 +0200

    l10-ex7-swipe_delete_ui_2

--- Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
+++ Lesson09-ToDo-List/T09.07-Exercise-SwipeToDelete/app/src/main/java/com/example/android/todolist/MainActivity.java
@@ -16,10 +16,12 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
@@ -218,4 +220,3 @@ public class MainActivity extends AppCompatActivity implements
         }
 
 }
-
